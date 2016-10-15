dc64a9bb5ba6bdba9aab8ae38caeea4dc98a8ad7
Matt Oliver <mattoliver.mattoliver@gmail.com>
2016-10-15 18:25:04 -0500

    fixing syntax errors

--- app.js
+++ app.js
@@ -8,16 +8,16 @@ var arrOfMagicSayings = [
   "It's so important. We can change lives. https://goo.gl/3TXKOa #RiggedSystem #TrumpTrain",
   "The security aspect of cyber is very, very tough. And maybe it's hardly doable! https://goo.gl/f3qXJF",
   "We have so many things that we have to do better! Certainly cyber is one of them! https://goo.gl/wIir0Y",
-  "There's never been anybody in the history of US politics so abusive to women! #WomenForTrump!"
+  "There's never been anybody in the history of US politics so abusive to women! #WomenForTrump!",
   "#WomenForTrump: RT this and get the word out there! https://goo.gl/dQCRmR",
   "I'm counting on you to RT and get this out there https://goo.gl/4utLRA #TrumpTrain",
-  "I need everyone on the #TrumpTrain to RT this and get this spread! https://goo.gl/S1gLTe #MAGA"
+  "I need everyone on the #TrumpTrain to RT this and get this spread! https://goo.gl/S1gLTe #MAGA",
   "I'll have a new legacy in months after the world is broken! https://goo.gl/f3qXJF",
   "I need your help getting this out far and wide! #TrumpTrain #WikiLeaks https://goo.gl/f3qXJF",
   "My strategy to defeat #ISIS is secret! Click here, RT and spread this! https://goo.gl/f3qXJF",
   "Help me spread truth on #CrookedHillary and 2nd Amendment! RT this! https://goo.gl/Zn6CSv",
   "Lies! Will you get me in Oval Office? RT this and get the word out! https://goo.gl/eiIM0Z",
-  "Corrupt #BiasedMedia don't want this 2 spread! Help get it out there! https://goo.gl/xNeVic"
+  "Corrupt #BiasedMedia don't want this 2 spread! Help get it out there! https://goo.gl/xNeVic",
   "#WomenForTrump: #BiasedMedia trying 2 keep this from spreading! RT it and #MAGA! https://goo.gl/dQCRmR",
   "#TrumpTrain: let's send a message to the #BiasedMedia and spread this far and wide: https://goo.gl/wIir0Y",
   "I need #TrumpsArmy 2 send a message to the #BiasedMedia. RT and share! https://goo.gl/wIir0Y",
